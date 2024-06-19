4495f5109ffd8896b2e2adb35ae793cd8c647e52
hello-anxiety <pitpibool112@gmail.com>
2024-06-19 11:56:57 +0700

    ส่งการบ้านครั้งที่ 1

--- assignment1/thread01.py
+++ assignment1/thread01.py
@@ -2,14 +2,17 @@
 from time import sleep, ctime
 from threading import Thread
 
+# a custom fuction that blocks for amoment
 def task():
+    #block for a moment
     sleep(1)
-
+    #display a message
     print(f'{ctime()} This is from another thread')
 
+#create a thread
 thread = Thread(target=task)
-
+#run thread
 thread.start()
-
+#wait for thread to finnish
 print(f'{ctime()} Waiting for the thred...')
 thread.join()
--- assignment1/thread02.py
+++ assignment1/thread02.py
@@ -1,1 +1,18 @@
 # running a function with arguments in another thread
+from time import sleep, ctime
+from threading import Thread
+
+# a custom fuction that blocks for amoment
+def task(sleep_time,message):
+    #block for a moment
+    sleep(sleep_time)
+    #display a message
+    print(f'{ctime()} {message}')
+
+#create a thread
+thread = Thread(target=task,args=(1.5,'New message from another thread'))
+#run thread
+thread.start()
+#wait for thread to finnish
+print(f'{ctime()} Waitting for the thread...')
+thread.join()
