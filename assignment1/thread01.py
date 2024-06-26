f52e6d0defc4d9d7ea3646300a758b6e6ce7433f
hello-anxiety <pitpibool112@gmail.com>
2024-06-26 16:40:02 +0700

    ส่งงานครั้งที่ 2

--- assignment1/thread01.py
+++ assignment1/thread01.py
@@ -2,6 +2,7 @@
 from time import sleep, ctime
 from threading import Thread
 
+<<<<<<< HEAD
 # a custom fuction that blocks for amoment
 def task():
     #block for a moment
@@ -15,4 +16,19 @@ thread = Thread(target=task)
 thread.start()
 #wait for thread to finnish
 print(f'{ctime()} Waiting for the thred...')
+=======
+# a custom function that blocks for a moment
+def task():
+    # block for a moment
+    sleep(1)
+    # display a message
+    print(f'{ctime()} This is from another thread')
+
+# create a thread
+thread = Thread(target=task)
+# run the thread
+thread.start()
+# wait for the thread to finish
+print(f'{ctime()} Waiting for the thread...')
+>>>>>>> 3970926c92ff939fa8cee033ec5ab6e687e5eeab
 thread.join()
--- assignment1/thread02.py
+++ assignment1/thread02.py
@@ -2,6 +2,7 @@
 from time import sleep, ctime
 from threading import Thread
 
+<<<<<<< HEAD
 # a custom fuction that blocks for amoment
 def task(sleep_time,message):
     #block for a moment
@@ -16,3 +17,19 @@ thread.start()
 #wait for thread to finnish
 print(f'{ctime()} Waitting for the thread...')
 thread.join()
+=======
+# a custom function that blocks for a moment
+def task(sleep_time, message):
+    # block for a moment
+    sleep(sleep_time)
+    # display a message
+    print(f'{ctime()} {message}')
+
+# create a thread
+thread = Thread(target=task, args=(1.5, 'New message from another thread'))
+# run the thread
+thread.start()
+# wait for the thread to finish
+print(f'{ctime()} Waiting for the thread...')
+thread.join()
+>>>>>>> 3970926c92ff939fa8cee033ec5ab6e687e5eeab
